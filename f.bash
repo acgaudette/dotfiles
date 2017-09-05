a993d1225ed695f3acb2f22df87b8e29b16c84e9
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-09-05 15:15:01 -0400

    Patch dest

--- f.bash
+++ f.bash
@@ -3,16 +3,16 @@
 function link {
   local -r file=$1; local -r target=$2
 
-  local -r path=$(readlink -f $file)
-  local -r dest=$target/$file
+  local -r path=$(readlink -f "$file")
+  local -r dest=$target/$(basename "$file")
 
   printf "Linking $file"
-  if [[ -L $dest ]]; then
+  if [ -L "$dest" ]; then
     printf " (overwriting...)"
   fi
 
-  if ln -sf $path $target
-    then printf "\n $(file $dest)\n"
+  if ln -sf "$path" "$target"
+    then printf "\n $(file "$dest")\n"
     else return 1
   fi
 }
@@ -20,17 +20,17 @@ function link {
 function link_dir {
   local -r dir=$1; local -r target=$2
 
-  if [ ! -d $dir ]; then
+  if [ ! -d "$dir" ]; then
     printf "Invalid target directory \"$dir\"\n" >&2
     return 1
   fi
 
-  if [ -z "$(ls -A $dir)" ]; then
+  if [ -z "$(ls -A "$dir")" ]; then
     printf "Target directory \"$dir\" is empty!\n" >&2
     return 1
   fi
 
-  for file in $(ls -A $dir); do
-    if ! link $dir/$file $target; then return 1; fi
+  for file in $(ls -A "$dir"); do
+    if ! link "$dir/$file" "$target"; then return 1; fi
   done; echo
 }
